dee040ed6673e2f278e488fae9b8546458bc589e
DJSwitchCase <64858642+DJSwitchCase@users.noreply.github.com>
2020-09-08 09:39:24 +0300

    Laba2 - Sokolovsky

---
+++ Laba2 - Sokolovsky/src/Circle.java
@@ -0,0 +1,12 @@
+public class Circle {
+    public int radius;
+
+    public int getRadius()
+    {
+        return radius;
+    }
+    public void setRadius(int r)
+    {
+        radius=r;
+    }
+}
---
+++ Laba2 - Sokolovsky/src/CircleTest.java
@@ -0,0 +1,7 @@
+public class CircleTest {
+    public static void main(String[] args) {
+        Circle circ = new Circle();
+        circ.setRadius(4);
+        System.out.println(circ.getRadius());
+    }
+}
