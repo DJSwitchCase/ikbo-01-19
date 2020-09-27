ea218b75191c4a0f6ad02760c718c8ad8db1407c
DJSwitchCase <64858642+DJSwitchCase@users.noreply.github.com>
2020-09-27 17:20:06 +0300

    1412

---
+++ Practica4/src/Practica4.java
@@ -0,0 +1,60 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class Practica4 extends JFrame
+{
+    int mil=0;
+    int mad=0;
+JButton milan = new JButton("AC Milan");
+JButton madrid = new JButton("Real Madrid");
+JLabel milLab = new JLabel("Result: "+ mil+ "X "+mad);
+JLabel madlab = new JLabel("Last Scorer: N/A");
+JLabel winnerlab = new JLabel("WINNER: draw");
+
+Practica4()
+{
+    super("Window");
+    setLayout(new FlowLayout());
+    setSize(400, 500);
+    add(milan);
+    add(madrid);
+    add (milLab);
+    add (madlab);
+    add(winnerlab);
+    milan.addActionListener(new AbstractAction() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            mil+=1;
+            milLab.setText("Result: "+ mil+ "X"+mad);
+            madlab.setText("Last scorer: Milan");
+            if (mil>mad)
+                winnerlab.setText("Milan");
+            else if (mil==mad)
+                winnerlab.setText("Draw");
+            else if (mil<mad)
+                winnerlab.setText("Madrid");
+
+        }
+    });
+    madrid.addActionListener(new AbstractAction() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            mad+=1;
+            milLab.setText("Result: "+ mil+ "X"+mad);
+            madlab.setText("Last scorer: Madrid");
+            if (mil>mad)
+                winnerlab.setText("Winner: Milan");
+            else if (mil==mad)
+                winnerlab.setText("Winner: Draw");
+            else if (mil<mad)
+                winnerlab.setText("Winner: Madrid");
+        }
+    });
+
+
+}
+    public static void main(String[] args) {
+        new Practica4().setVisible(true);
+    }
+}
