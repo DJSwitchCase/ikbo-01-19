0fdfdc9cf44cf1525a178cbd117c4b93c0907a55
DJSwitchCase <64858642+DJSwitchCase@users.noreply.github.com>
2020-11-25 01:37:59 +0300

    5

--- Laba8 - Sokolovsky/src/com/company/Main.java
+++ Laba8 - Sokolovsky/src/com/company/Main.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
         //Ввод с клавы
         Scanner sc =  new Scanner(System.in);
