a1a6d18892ef37a718fbf2b74a3c2efaf2e11ee2
DJSwitchCase <LighterST@yandex.ru>
2021-03-02 22:13:58 +0300

    3

---
+++ Practica7 - Sokolovsky/src/z2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.*;
+public class z2 {
+    public static void main(String[] args) {
+        int x=5;
+        int y=6;
+        System. out. println("x=" +x+ y + "; у=" + y);
+        }
+    };
+
